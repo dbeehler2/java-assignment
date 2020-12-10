4f4d340d77bb551705d6f3322f8dd388a431d748
dbeehler2 <73602672+dbeehler2@users.noreply.github.com>
2020-12-09 22:14:34 -0500

    Java Assignment

---
+++ JavaAssignment/src/MyPackage/User.java
@@ -0,0 +1,26 @@
+package MyPackage;
+
+public class User {
+	
+	public String firstName;
+	public String lastName;
+	public int age;
+	public String gender;
+	
+	
+	public static void main(String[] args) { 
+
+		String firstName = "Bob";
+		String lastName = "Smith";
+		int age = 59;
+		String gender = "Male";
+		
+		
+		System.out.println("This is the firstName field " + firstName);
+		System.out.println("This is the lastName field " + lastName);
+		System.out.println("This is the field " + age);
+		System.out.println("This is the gender field " + gender);
+	}
+
+
+}
